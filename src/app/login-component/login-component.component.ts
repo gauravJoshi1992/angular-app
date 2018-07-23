168776234481c605ce2aa15c08b0a665a0ccca1b
Gaurav Joshi <gaurav@webcontentor.com>
2018-07-23 18:10:41 +0530

    Login page validation work

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -8,27 +8,35 @@ import * as _ from 'lodash';
 })
 export class LoginComponentComponent implements OnInit {
 
-  	constructor() { }
+    constructor() { }
     isNewUser = false;
     isSubmitBtnEnable = false;
     isBtnEnable = 'disabled';
-    labels = {};
 
-    public createDataObject () {
-        this.labels = {
-            loginHeading: 'Enter UserName and Password to login.',
-            createUserHeading: 'Enter UserName and Password to register.',
-            UserName: 'UserName',
-            email: 'Email',
-            password: 'Password',
-            reEnterPassword: 'Re-enter Password',
-            newUser: 'New User',
-            submit: 'Submit',
-        }
-    }
+    labels = {
+        loginHeading: 'Enter UserName and Password to login.',
+        createUserHeading: 'Enter UserName and Password to register.',
+        newUser: 'New User',
+        submit: 'Submit',
+    };
+
+    loginInputData = [
+        {
+            display: 'UserName',
+            errorMsg: 'UserName is required',
+            showErrorBox: false,
+            type: 'normal',
+            validationFun: this.userNameValidation,
+        }, {
+            display: 'Password',
+            errorMsg: 'Password is required and at-least of 8 char',
+            showErrorBox: false,
+            type: 'password',
+            validationFun: this.passwordValidation,
+        },
+    ];
 
     ngOnInit() {
-        this.createDataObject();
         this.submitBtnClass();
     }
 
@@ -40,8 +48,44 @@ export class LoginComponentComponent implements OnInit {
         this.isBtnEnable = this.isSubmitBtnEnable ? '' : 'disabled';
     }
 
-    public dnOnChange (searchValue : string) {
-        this.isSubmitBtnEnable = !_.isEmpty(searchValue) ? true : false;
+    public setvalidation () {
+        _.each(this.loginInputData, function (obj) {
+            obj.showErrorBox = obj.validationFun(obj);
+        })
+    }
+
+    public userNameValidation (obj) {
+        if (_.isEmpty(obj.value)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public passwordValidation (obj) {
+        if (_.isEmpty(obj.value) || obj.value.length < 8) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public doOnChange (target, data) {
+        data.value = target.value;
+
+        var isTruevalue = this.isEveryValueTrue(this.loginInputData);
+
+        this.isSubmitBtnEnable = !_.isEmpty(isTruevalue);
         this.submitBtnClass();
+    }
+
+    public submitData () {
+        this.setvalidation();
+    }
+
+    public isEveryValueTrue (obj) {
+        return _.filter(obj, function (objValue) {
+            return !_.isEmpty(objValue.value);
+        });
     }
 }
